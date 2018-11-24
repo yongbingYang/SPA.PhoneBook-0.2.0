e2ad777110ced964f9c48796fd054f868a35b4c6
杨勇兵 <yongbing.yang@xc-smart.cn>
2018-11-24 09:08:49 +0800

    提交  Modal 的添加
    提交  Modal 的添加

--- src/PhoneBook-angular/src/app/persons/persons.component.ts
+++ src/PhoneBook-angular/src/app/persons/persons.component.ts
@@ -1,11 +1,14 @@
 import { Component, OnInit, Injector } from '@angular/core';
 import { PagedListingComponentBase, PagedRequestDto } from '@shared/paged-listing-component-base';
+import { appModuleAnimation } from '@shared/animations/routerTransition';
 import { PersonListDto, PersonServiceProxy, PagedResultDtoOfPersonListDto } from '@shared/service-proxies/service-proxies';
+import { appendFile } from 'fs';
 
 @Component({
   selector: 'app-persons',
   templateUrl: './persons.component.html',
-  styleUrls: ['./persons.component.css']
+  styleUrls: ['./persons.component.css'],
+  animations: [appModuleAnimation()]
 })
 export class PersonsComponent extends PagedListingComponentBase<PersonListDto> implements OnInit {
 
@@ -19,14 +22,14 @@ people:PersonListDto[] = [];
     private _personService:PersonServiceProxy
   ) { 
     super(injector);
+    
   }
 
-  ngOnInit() {
+  ngOnInit():void{
+    super.ngOnInit();
   }
-
 
   protected list(request: PagedRequestDto, pageNumber: number, finishedCallback: Function): void {
-      
     
     this._personService.getPagedPersons(this.filter,'Id',request.maxResultCount,request.skipCount).finally(()=>{
       finishedCallback();
